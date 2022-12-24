07edd17eed60d36aeaeba28fdf276d4622f3a506
zlaval <zlaval@gmail.com>
2022-12-24 20:09:06 +0100

    feat: add db connection
    * postgres driver

--- framework/go.mod
+++ framework/go.mod
@@ -7,5 +7,14 @@ require (
 	github.com/CloudyKit/jet/v6 v6.2.0 // indirect
 	github.com/alexedwards/scs/v2 v2.5.0 // indirect
 	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.13.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgx/v5 v5.2.0 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20220829220503-c86fa9a7ed90 // indirect
+	golang.org/x/text v0.3.8 // indirect
 )
--- framework/main.go
+++ framework/main.go
@@ -28,6 +28,7 @@ type Goravel struct {
 	Render   *render.Render
 	JetViews *jet.Set
 	Session  *scs.SessionManager
+	DB       Database
 	config   config
 }
 
@@ -36,6 +37,7 @@ type config struct {
 	renderer    string
 	cookie      cookieConfig
 	sessionType string
+	database    databaseConfig
 }
 
 func (g *Goravel) New(rootPath string) error {
@@ -59,6 +61,19 @@ func (g *Goravel) New(rootPath string) error {
 	}
 
 	infoLog, errorLog := g.startLoggers()
+
+	dbType := os.Getenv("DATABASE_TYPE")
+	if dbType != "" {
+		db, err := g.OpenDB(dbType, g.BuildDSN())
+		if err != nil {
+			errorLog.Println(err)
+			os.Exit(1)
+		}
+		g.DB = Database{
+			databaseType: dbType,
+			Pool:         db,
+		}
+	}
 
 	g.ErrorLog = errorLog
 	g.InfoLog = infoLog
@@ -148,4 +163,26 @@ func (g *Goravel) createRenderer() {
 		JetViews: g.JetViews,
 	}
 	g.Render = &renderer
+}
+
+func (g *Goravel) BuildDSN() string {
+	var dsn string
+
+	switch os.Getenv("DATABASE_TYPE") {
+	case "postgres", "postgresql":
+		dsn = fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s timezone=UTC connect_timeout=5",
+			os.Getenv("DATABASE_HOST"),
+			os.Getenv("DATABASE_PORT"),
+			os.Getenv("DATABASE_USER"),
+			os.Getenv("DATABASE_NAME"),
+			os.Getenv("DATABASE_SSL_MODE"),
+		)
+		password := os.Getenv("DATABASE_PASS")
+		if password != "" {
+			dsn = fmt.Sprintf("%s password=%s", dsn, password)
+		}
+	default:
+
+	}
+	return dsn
 }
